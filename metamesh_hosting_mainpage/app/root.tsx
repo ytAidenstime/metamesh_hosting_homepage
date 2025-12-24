3013977af6707fe3272c763b206a1e106f173fe6
ytAidenstime <99297721+ytAidenstime@users.noreply.github.com>
2025-12-24 16:22:46 -0500

    Fixed

--- metamesh_hosting_mainpage/app/root.tsx
+++ metamesh_hosting_mainpage/app/root.tsx
@@ -26,26 +26,32 @@ export const links: Route.LinksFunction = () => [
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
+    <head>
+      <meta charSet="utf-8"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <Meta/>
+      <link rel="icon" type="image/png" href="favicon.png"/>
+      <meta property="og:title" content="MetaMesh"/>
+      <meta property="og:description" content="Providing high quality game server, vps, and cpanel hosting to our clients."/>
+      <meta property="og:image" content="https://i.imgur.com/3vVjDXA.png"/>
+      <meta property="og:url" content="https://metamesh.ca"/>
+      <meta name="theme-color" content="#02A7AE"/>
+      <Links/>
+    </head>
+    <body>
+    {children}
+    <ScrollRestoration/>
+    <Scripts/>
+    </body>
     </html>
   );
 }
 
 export default function App() {
-  return <Outlet />;
+  return <Outlet/>;
 }
 
-export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+export function ErrorBoundary({error}: Route.ErrorBoundaryProps) {
   let message = "Oops!";
   let details = "An unexpected error occurred.";
   let stack: string | undefined;
